1482ff97908a9de6e6fab9712f4dc816cc8d94a9
yelinko20 <119092367+yelinko20@users.noreply.github.com>
2023-05-15 17:48:21 +0630

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,18 @@
-
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
-import {HiMenuAlt4} from "react-icons/hi"
+import { HiMenuAlt4 } from "react-icons/hi";
 
 export default function Navbar() {
   const [token, setToken] = useState(localStorage.getItem("token"));
   const [showMenu, setShowMenu] = useState(false);
-  const [navScroll,setNavScroll] = useState(false)
+  const [navScroll, setNavScroll] = useState(false);
   useEffect(() => {
     function scrollActive() {
-      setNavScroll(window.scrollY > 20)
+      setNavScroll(window.scrollY > 20);
     }
-    window.addEventListener("scroll", scrollActive)
-    return () => window.removeEventListener("scroll", scrollActive)
-  },[])
+    window.addEventListener("scroll", scrollActive);
+    return () => window.removeEventListener("scroll", scrollActive);
+  }, []);
 
   useEffect(() => {
     setToken(localStorage.getItem("token"));
@@ -27,19 +26,26 @@ export default function Navbar() {
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
+  function handleBlur() {
+    setShowMenu(!showMenu);
+  }
 
   return (
-    <header className={`fixed w-full top-0 left-0 z-20 p-4 ${navScroll && "bg-White shadow-md"}`}>
+    <header
+      className={`fixed w-full top-0 left-0 z-20 p-4 ${
+        navScroll && "bg-White shadow-md"
+      }`}
+    >
       <nav className="container mx-auto flex justify-between items-center">
         <Link to="/">
           <div className="text-2xl font-medium">Blogpedia</div>
         </Link>
-        <div className="sm:hidden">
+        <div className="sm:hidden" onBlur={handleBlur}>
           <button
             onClick={toggleMenu}
             className="p-2 rounded-full  bg-secondary"
           >
-            <HiMenuAlt4 className="text-2xl"/>
+            <HiMenuAlt4 className="text-2xl" />
           </button>
           {showMenu && (
             <div className="absolute top-full right-4 py-2 bg-white rounded-lg shadow-md">
